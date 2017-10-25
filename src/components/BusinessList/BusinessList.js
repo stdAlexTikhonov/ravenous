f07e0031df68fb9eae0c6134e75dcd0e54a24b1a
stdAlexTikhonov <stdAlexTikhonov@hotmail.com>
2017-10-25 22:51:35 +0300

    init commit

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(function(business, index) {
+          return <Business key={index} business={business}/>
+        })
+      }
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
---
+++ src/util/Yelp.js
@@ -0,0 +1,10 @@
+const clientId = 'kSRVfeOdBS_i3hf3DRMvAw';
+const secret = '3XAeVud64VDb8jH2Lbg9ei1NVwqGUS0qIb3L732Mfxht7eiyd1znUxZVAWse6C8V';
+
+let accessToken;
+
+let Yelp = {};
+
+Yelp.getAccessToken = function() {
+  
+}
