afdb51386a40ee889c5fba1af33fd31f197fa785
stdAlexTikhonov <stdAlexTikhonov@hotmail.com>
2017-10-27 11:42:49 +0300

    yelp api

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -5,6 +5,44 @@ let accessToken;
 
 let Yelp = {};
 
-Yelp.getAccessToken = function() {
-  
-}
+Yelp.getAccessToken = function(accessToken) {
+  if (accessToken) {
+    return new Promise(resolve => resolve(accessToken));
+  }
+  return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_creditials&client_id=${clientId}&client_secret=${secret}`,
+                {
+                  method: 'POST'
+                }).then(response => response.json())
+                  .then(jsonResponse => accessToken = jsonResponse.accessToken);
+};
+
+Yelp.search = function(term, location, sortBy) {
+  return Yelp.getAccessToken().then(() => fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=
+    ${term}&location=${location}&sortBy=${sortBy}`), 
+    { headers: 
+      { Authorization: `Bearer ${accessToken}`}
+    }).then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.businesses) {
+          jsonResponse.businesses.map(
+            business => {
+              return {
+                id: business.id,
+                imageSrc: business.image_url,
+                name: business.name,
+                address: business.location.address1,
+                city: business.location.city,
+                state: business.location.state,
+                zipCode: business.location.zip_code,
+                category: business.categories.title,
+                rating: business.rating,
+                reviewCount: business.review_count
+              }
+            }
+          );
+        }
+      });
+};
+
+
+export default Yelp;
